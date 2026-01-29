f80de4dd2608a63d5e058dc1a50d94225e66d4a1
kauediniz-dev <dinizkaue26@gmail.com>
2026-01-28 23:38:00 -0300

    cards

---
+++ src/app/[slug]/components/consumption-method-option.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { ConsumptionMethod } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ConsumptionMethodOptionProps {
+  imageUrl: string;
+  imageAlt: string;
+  buttonText: string;
+  option: ConsumptionMethod
+}
+
+const ConsumptionMethodOption = ({
+    imageUrl,
+    imageAlt,
+    buttonText,
+    option
+
+}: ConsumptionMethodOptionProps) => {
+    return ( <Card>
+            <CardContent className="flex flex-col items-center gap-8 py-8">
+                <div className="relative h-[80px] w-[78px]">
+                    <Image src={imageUrl} fill alt={imageAlt} className="object-contain"/>
+                </div>
+                <Button variant="secondary" className="rounded-full">
+                    <Link href={`/menu?consumptionMethod=${option}`}>
+                        {buttonText}
+                    </Link>
+                </Button>
+            </CardContent>
+        </Card> );
+}
+
+export default ConsumptionMethodOption;
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,29 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
-import { db } from "@/lib/prisma";
-import { get } from "http";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import ConsumptionMethodOption from "./components/consumption-method-option";
 
 interface RestaurantPageProps {
-    params: Promise<{ slug: string }>;  
+  params: {
+    slug: string;
+  };
 }
 
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
-    const { slug } = await params;
-    const restaurant = await getRestaurantBySlug(slug);
-    if (!restaurant) {
-        return notFound;
-    }
+  const { slug } = params;
 
-    return (
-        <div className="h-screen flex flex-col items-center justify-center px-6 pt-24">
-            <div className="flex flex-col items-center gap-2">
-                <Image src={restaurant?.avatarImageUrl} alt={restaurant.name} width={82} height={82} />
-                <h2 className="font-semibold">
-                    {restaurant.name}
-                </h2>
-            </div>
-        </div>
-    );
+  const restaurant = await getRestaurantBySlug(slug);
+
+  if (!restaurant) {
+    notFound();
+  }
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center px-6 pt-24">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+
+      <div className="p-24 text-center space-y-2">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos oferecendo
+          praticidade e sabor em cada detalhe
+        </p>
+      </div>
+      <div className="pt-14 grid grid-cols-2 gap-4">
+        <ConsumptionMethodOption
+            buttonText="Para comer aqui"
+            imageUrl="/dine_in.png"
+            imageAlt="Comer aqui"
+            option="DINE_IN"
+        />
+        <ConsumptionMethodOption
+            buttonText="Para levar"
+            imageUrl="/take_way.png"
+            imageAlt="Para levar"
+            option="TAKEWAY"
+        />
+      </div>
+    </div>
+  );
 };
+
 export default RestaurantPage;

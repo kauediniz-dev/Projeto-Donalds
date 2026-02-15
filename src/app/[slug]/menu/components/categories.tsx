ff17e28d08da1b452ba763fd8bef1a64dd213c7f
kauediniz-dev <dinizkaue26@gmail.com>
2026-02-15 19:44:56 -0300

    feat: ensure cart summary is show on restaurant page

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -1,13 +1,17 @@
 "use client";
-import {  Prisma, Restaurant } from "@prisma/client";
-import { ClockIcon } from "lucide-react";
+import { Prisma, Restaurant } from "@prisma/client";
+import { Car, ClockIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 import Products from "./products";
+import { CartContext } from "../contexts/cart";
+import { format } from "path";
+import { formatCurrent } from "@/helpers/format-current";
+import CartSheet from "./cart-sheet";
 
 interface RestaurantCategoriesProps {
     restaurante: Prisma.RestaurantGetPayload<{
@@ -31,6 +35,7 @@ const RestaurantCategories = ({ restaurante }: RestaurantCategoriesProps) => {
     const [selectedCategory, setSelectedCategory] = useState<MenuCategoryWithProducts>(
         restaurante.menuCategory[0]
     );
+    const { products, total, toggleCart, totalQuantity } = useContext(CartContext);
     const handleCategoryClick = (category: MenuCategoryWithProducts) => {
         setSelectedCategory(category);
     }
@@ -42,7 +47,7 @@ const RestaurantCategories = ({ restaurante }: RestaurantCategoriesProps) => {
         <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl  bg-white">
             <div className="p-5">
                 <div className="flex items-center gap-3 ">
-                    <Image 
+                    <Image
                         src={restaurante.avatarImageUrl}
                         alt={restaurante.name}
                         height={45}
@@ -52,25 +57,39 @@ const RestaurantCategories = ({ restaurante }: RestaurantCategoriesProps) => {
                         <h2 className="text-lg font-semibold">{restaurante.name}</h2>
                         <p className="text-xs opacity-55">{restaurante.description}</p>
                     </div>
-                </div>    
+                </div>
                 <div className="flex items-center gap-1 text-xs text-green-500 mt-3">
                     <ClockIcon size={12} />
                     <p>Aberto!</p>
                 </div>
-                    <ScrollArea className="w-full">
-                        <div className="flex w-max space-x-4 pb-2 pt-3 p-1">
-                            {restaurante.menuCategory.map(category => (
-                                <Button onClick={() => handleCategoryClick(category)} key={category.id} variant={getCategoryButtonVariant(category)}
-                                    size="sm" className="rounded-full">
-                                    {category.name}
-                                </Button>
-                            ))}
-                        </div>
-                        <ScrollBar orientation="horizontal" />
-                    </ScrollArea>
+                <ScrollArea className="w-full">
+                    <div className="flex w-max space-x-4 pb-2 pt-3 p-1">
+                        {restaurante.menuCategory.map(category => (
+                            <Button onClick={() => handleCategoryClick(category)} key={category.id} variant={getCategoryButtonVariant(category)}
+                                size="sm" className="rounded-full">
+                                {category.name}
+                            </Button>
+                        ))}
+                    </div>
+                    <ScrollBar orientation="horizontal" />
+                </ScrollArea>
 
-                    <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
-                    <Products products={selectedCategory.products} />
+                <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
+                <Products products={selectedCategory.products} />
+                {products.length > 0 && (
+                    <div className="fixed bottom-0 z-50 flex w-full items-center justify-between border-t bg-white px-5 py-2">
+                        <div className="flex items-center gap-4">
+                            <p className="text-xs text-muted-foreground">Total dos pedidos</p>
+                            <p className="text-sm font-semibold">{formatCurrent(total)}
+                                <span className="text-xs text-muted-foreground">
+                                    / {totalQuantity} {totalQuantity === 1 ? "item" : "itens"}
+                                </span>
+                            </p>
+                        </div>
+                        <Button onClick={toggleCart}>Ver sacola</Button>
+                        <CartSheet />
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -10,6 +10,7 @@ export interface ICartContext {
     isOpen: boolean;
     products: CartProduct[];
     total: number;
+    totalQuantity: number;
     toggleCart: () => void;
     addProduct: (product: CartProduct) => void;
     decreaseProductQuantity: (productId: string) => void;
@@ -21,6 +22,7 @@ export const CartContext = createContext<ICartContext>({
     isOpen: false,
     products: [],
     total: 0,
+    totalQuantity: 0,
     toggleCart: () => { },
     addProduct: () => { },
     decreaseProductQuantity: () => { },
@@ -34,6 +36,9 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
 
     const total = products.reduce((acc, product) => {
         return acc + (product.price * product.quantity);
+    }, 0);
+    const totalQuantity = products.reduce((acc, product) => {
+        return acc + product.quantity;
     }, 0);
     const toggleCart = () => {
         setIsOpen((prev) => !prev);
@@ -93,6 +98,7 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
             isOpen,
             products,
             total,
+            totalQuantity,
             toggleCart,
             addProduct,
             decreaseProductQuantity,
